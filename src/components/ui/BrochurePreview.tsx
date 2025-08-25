00835b4ad300b39e300455035ba3c1129064c10c
Paolo <paolo.herrera.araya@gmail.com>
2025-08-25 19:40:36 -0400

    refactor(BrochurePreview): Mejora del código iframe - Llamada a la api de descarga de brochure - Se añade max-w-full del iframe para responsividad.

--- src/components/ui/BrochurePreview.tsx
+++ src/components/ui/BrochurePreview.tsx
@@ -1,10 +1,8 @@
 import { Button } from '@heroui/react'
 import { FileDown } from 'lucide-react'
-import { useBrochureStore } from "../../stores/useBrochureStore";
-import { useLanguageStore } from "../../stores/useLanguageStore";
-import axios from 'axios';
-
-
+import { useBrochureStore } from '../../stores/useBrochureStore'
+import { useLanguageStore } from '../../stores/useLanguageStore'
+import axios from 'axios'
 
 export const BrochurePreview = () => {
   const { brochure, cacheKey } = useBrochureStore()
@@ -14,24 +12,27 @@ export const BrochurePreview = () => {
   const titleLabel = language === 'es' ? 'Vista previa del folleto' : 'Brochure preview'
 
   const handleDownloadPdf = async () => {
-    
     try {
       /* ENVÍO DE LAS DATOS AL SERVIDOR */
 
       // Se obtiene la respuesta del servidor. El archivo PDF se descarga en el navegador
-      const response = await axios.post('http://localhost:8000/api/v1/download_brochure_pdf', {
-        "cache_key": cacheKey,
-      }, {
-        responseType: 'blob',
-        headers: {
-          'Accept': 'application/pdf',
+      const response = await axios.post(
+        'http://localhost:8000/api/v1/download_brochure_pdf',
+        {
+          cache_key: cacheKey,
         },
-      })
-      
+        {
+          responseType: 'blob',
+          headers: {
+            Accept: 'application/pdf',
+          },
+        }
+      )
+
       // Obtener el nombre del archivo
       const disposition = response.headers['content-disposition']
       let filename = 'brochure.pdf'
-      
+
       // Si el nombre del archivo está en la cabecera de la respuesta, entonces se extrae
       if (disposition && disposition.includes('filename=')) {
         filename = disposition.split('filename=')[1].split(';')[0]
@@ -66,7 +67,7 @@ export const BrochurePreview = () => {
         </Button>
       </div>
       <iframe
-        className="w-full min-h-[24rem] h-[70vh] bg-white overflow-auto"
+        className="block w-full max-w-full min-h-[24rem] h-[70vh] bg-white overflow-auto"
         title="Brochure preview"
         srcDoc={brochure ?? ''}
         sandbox=""
