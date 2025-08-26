2ed3707d07d9121bfd0485a55f33b1b6efd4dbb3
Paolo <paolo.herrera.araya@gmail.com>
2025-08-26 19:02:10 -0400

    feat (refactor): Se añaden los compoenentes de Conteiner, HowItWorksSection, FeaturesSection, OpenSourceSection, FAQSection y Footer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import './App.css'
 import { HeroSection } from './components/sections/HeroSection'
 import { NavbarUI } from './components/ui/NavbarUI'
+import { HowItWorksSection } from './components/sections/HowItWorksSection'
+import { FeaturesSection } from './components/sections/FeaturesSection'
+import { OpenSourceSection } from './components/sections/OpenSourceSection'
+import { FAQSection } from './components/sections/FAQSection'
+import { Footer } from './components/ui/Footer'
+import { Container } from './components/ui/Container'
 
 function App() {
   return (
     <>
-      <NavbarUI />
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-slate-950">
-        <HeroSection />
+      <div className="bg-gray-100 dark:bg-slate-950">
+        <Container>
+          <NavbarUI />
+          <HeroSection />
+          <HowItWorksSection />
+          <FeaturesSection />
+          <OpenSourceSection />
+          <FAQSection />
+          <Footer />
+        </Container>
       </div>
     </>
   )
