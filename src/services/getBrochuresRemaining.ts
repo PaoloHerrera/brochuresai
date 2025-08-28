39fb5a559b34d3d5d0914a72b7db0d2a5622fb99
Paolo <paolo.herrera.araya@gmail.com>
2025-08-28 19:52:30 -0400

    refactor(services): Uso de API_BASE_URL para el servicio de remaining

--- src/services/getBrochuresRemaining.ts
+++ src/services/getBrochuresRemaining.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { API_BASE_URL } from "../config";
 
 export const getBrochuresRemaining = async (anonUserId: string | null) => {
   try {
-    const res = await axios.get('http://localhost:8000/api/v1/users/get_remaining', {
+    const res = await axios.get(`${API_BASE_URL}/api/v1/users/get_remaining`, {
       params: {
         anonUserId,
       },
