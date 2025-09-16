568dadcfdb1571f823710ab2601bcfda6c5f9401
Paolo Herrera <paolo.herrera.araya@gmail.com>
2025-09-16 19:01:02 -0300

    fix(i18n): add error translations and update usage in App (#22)
    - Add new app.ts file with error messages in English and Spanish
    - Update App.tsx to use new error message structure from app.ts

---
+++ src/lang/app.ts
@@ -0,0 +1,18 @@
+export const APP_TEXT = {
+  en: {
+    error: {
+      errorRemaining: {
+        title: 'Error',
+        description: 'Cannot get brochures remaining. Please try again later.',
+      }
+    }
+  },
+  es: {
+    error: {
+      errorRemaining: {
+        title: 'Error',
+        description: 'No se puede obtener el número de folletos disponibles. Por favor, inténtalo de nuevo más tarde.',
+      }
+    }
+  }
+}
